7f428c50158dbe24cd9ccf8d8f095b3c6818018a
ericplankton <154643303+ericplankton@users.noreply.github.com>
2023-12-30 16:18:49 +0300

    Add files via upload

---
+++ DivideScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DivideScript : MonoBehaviour
+{
+    public static class Divisors
+    {
+
+        public static int[] GetDivisorsMe(int n)
+        {
+            if (n <= 0)
+            {
+                return null;
+            }
+            List divisors = new List();
+            for (int i = 1; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    divisors.Add(i);
+                    if (i != n / i)
+                    {
+                        divisors.Add(n / i);
+                    }
+                }
+            }
+            divisors.Sort();
+            return divisors.ToArray();
+        }
+    }
+}
+
+
+   
+
